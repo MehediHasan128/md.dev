f72b56039428479db64a499b0bd059d8b0072ed1
MehediHasan128 <mehedihasan12926@gmail.com>
2026-05-03 12:32:57 +0600

    complete the tech stack section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/sections/Hero";
+import TechStack from "@/sections/TechStack";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       </div>
       <div>
         <Hero />
+        <TechStack />
       </div>
     </>
   );
